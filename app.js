2fa183ca5eef05f8e29f9c18db7ff08f18931bf6
Nikhil Jose <57913645+nikiljos@users.noreply.github.com>
2022-10-14 16:27:33 +0000

    v1.5

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-document.querySelectorAll(".logo").forEach(elt=>{
-  elt.onclick=()=>{
-    window.location="https://foodfolio.github.io/"
+document.querySelectorAll(".logo").forEach(elt => {
+  elt.onclick = () => {
+    window.location = "https://foodfolio.github.io/"
   }
 })
+
+let analyticScript = document.createElement("script");
+analyticScript.setAttribute("src", "https://cdn.splitbee.io/sb.js")
+document.body.append(analyticScript)
